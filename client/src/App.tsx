b476b01e71c201914a29df209b5ef2cafbdab3e6
Verifieddanny <dannyclassic56@gmail.com>
2025-03-24 17:37:27 +0100

    Feat: Implemented Mode Instruction & Game Master Loading Screen Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import GameMasterScreenContainer from "./components/GameMasterScreenContainer";
 function App() {
   return (
     <main className="relative w-screen min-h-screen bg-gradient-to-br from-zinc-900 via-neutral-900 to-slate-900 flex items-start mx-auto justify-center">
-      <div className="w-full h-full fixed z-[0] bg-[url('./assets/logo-bg.svg')] bg-repeat bg-center opacity-2"></div>
-      <div className="z-[1] absolute mx-auto w-full max-w-[600px] bg-[#1E1E1E] min-h-screen h-full border-l-2 border-r-2 border-dashed border-[#2D2D2D]"></div>
+      <div className="w-full h-full fixed z-[0] bg-[url('./assets/logo-bg.svg')] bg-repeat bg-center opacity-2">
+      </div>
+      <div className="z-[1] absolute mx-auto w-full max-w-[600px] bg-[#1E1E1E] min-h-screen h-full border-l-2 border-r-2 border-dashed border-[#2D2D2D]">
+        <GameMasterScreenContainer />
+      </div>
     </main>
   );
 }
