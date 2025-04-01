64feeacbafff1b3c25e1b61128f574accfec6451
Isaac Daniel <tissan300@gmail.com>
2025-04-01 21:10:19 +0100

    Added getstarted count down scrren and conected it to the current questions screen

--- client/src/components/solo-mode/Levels.tsx
+++ client/src/components/solo-mode/Levels.tsx
@@ -1,9 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import SynapzIcon from "./assets/game-master.svg";
 import HomeIcon from "./assets/home.svg";
 
 export default function Levels() {
   const levelsLabel = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
-
+  const navigate = useNavigate()
   return (
     <div className="Kumbh-sans w-full max-w-[600px] h-full flex flex-col gap-3 items-center justify-between  py-2 ">
       <div className=" w-full max-w-[345px] h-[608px] flex flex-col items-center justify-center gap-6   ">
@@ -15,6 +16,9 @@ export default function Levels() {
           {levelsLabel.reverse().map((level, index) => (
             <button
               key={index}
+              onClick={() => {
+                navigate(`/solo-mode/get-started/${level}`);
+              }}
               className="  flex items-center justify-center w-full h-[46px] rounded-sm p-3 bg-[#313131] border-[1px] border-[#454545] text-[#FFFFFF] font-normal text-sm cursor-pointer transition duration-200 transform hover:scale-105 "
             >
               Question {level}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,6 +11,8 @@ import SoloModeInstructions from "./pages/solo-mode/instructions.tsx";
 import SoloModeSelectMode from "./pages/solo-mode/select-mode.tsx";
 import SoloModeScore from "./pages/solo-mode/score.tsx";
 import SoloModeLevels from "./pages/solo-mode/levels.tsx";
+import SoloModeGetReady from "./pages/solo-mode/get-ready.tsx";
+import SoloModeCurrentQuestion from "./pages/solo-mode/current-question.tsx";
 
 const router = createBrowserRouter([
   {
@@ -44,10 +46,19 @@ const router = createBrowserRouter([
     element: <SoloModeLevels />,
   },
   {
+    path: "/solo-mode/get-started/:id",
+    element: <SoloModeGetReady />,
+  },
+  {
+    path: "/solo-mode/current-question/:id",
+    element: <SoloModeCurrentQuestion />,
+  },
+  {
     path: "/solo-mode/score",
     element: <SoloModeScore />,
   },
 ]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
